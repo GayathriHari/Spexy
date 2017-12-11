ca834d17a3f7b6fc777910d009eb5f0eee527c9c
GayathriHari <hari.gayathri6@gmail.com>
2017-12-11 12:00:01 +0530

    Spring boot structure created
    embedded db H2 is used

---
+++ spexydotcom/src/main/java/com/spexy/spexydotcom/SpexydotcomApplication.java
@@ -0,0 +1,12 @@
+package com.spexy.spexydotcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpexydotcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpexydotcomApplication.class, args);
+	}
+}
---
+++ spexydotcom/src/test/java/com/spexy/spexydotcom/SpexydotcomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spexy.spexydotcom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpexydotcomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
